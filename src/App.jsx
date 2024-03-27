58fdb2751560fe9a298e660d2ba9f8870f4c0a65
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-03-27 22:39:35 +0500

    imported css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import "./App.css";
 
-export const App = () => {
-  return <div>App</div>;
+const App = () => {
+  return <h1 className="underline 3xl">App</h1>;
 };
+
+export default App;
