8e40281b968176c1de40d26369cd955de37ff62e
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-03-30 19:54:50 +0500

    added navbar

--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -1,7 +1,9 @@
 import React, { createContext, useContext, useState } from "react";
 
+// Step 1: Create a new context called StateContext
 const StateContext = createContext();
 
+// Define the initial state for different application states
 const initialState = {
   chat: false,
   cart: false,
@@ -9,14 +11,33 @@ const initialState = {
   notification: false,
 };
 
+// Step 2: Define the ContextProvider component to manage state and provide context
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
 
+  // Function to handle click events and update isClicked state
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
+  // Provide state variables and functions to child components through Context.Provider
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+      }}
+    >
       {children}
     </StateContext.Provider>
   );
 };
 
+// Provide state variables and functions to child components via Context.Provider
 export const useStateContext = () => useContext(StateContext);
