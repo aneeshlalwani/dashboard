70f709c285215d8f0d9b90a519b0d2e54e9fbf99
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-04-07 09:24:34 +0500

    implemented pie chart

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesDirective,
+  AccumulationSeriesCollectionDirective,
+  PieSeries,
+  Inject,
+  Legend,
+} from "@syncfusion/ej2-react-charts";
+import { Header } from "../../components";
+import { pieChartData } from "../../data/dummy";
+import { Tooltip } from "@syncfusion/ej2-react-popups";
 
 const Pie = () => {
-  return <div>Pie</div>;
+  return (
+    <>
+      <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+        <Header
+          category="Pie Chart"
+          title="Comparison of Proportional values"
+        />
+        <AccumulationChartComponent id="charts">
+          <Inject services={[PieSeries, Legend, Tooltip]} />
+          <AccumulationSeriesCollectionDirective>
+            <AccumulationSeriesDirective
+              dataSource={pieChartData}
+              xName="x"
+              yName="y"
+              text="text"
+              type="Pie"
+              dataLabel={{
+                visible: true,
+                position: "Inside",
+                name: "text",
+              }}
+            />
+          </AccumulationSeriesCollectionDirective>
+        </AccumulationChartComponent>
+      </div>
+    </>
+  );
 };
 
 export default Pie;
